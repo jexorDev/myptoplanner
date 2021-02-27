f9bdef3e4ba40b049d4cce5d56ee4bf2cbbf0f28
Dustin Seger <dseger@efcsystems.com>
2021-02-27 15:21:22 -0600

    add google analytics

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import apexcharts from "./plugins/apexcharts"
+import VueGtag from "vue-gtag"
 import moment from "moment";
 
 Vue.config.productionTip = false
@@ -17,6 +18,10 @@ Vue.filter('formatDate', function (value) {
 
   return date.format("MM/DD/YYYY");
 })
+
+Vue.use(VueGtag, {
+  config: { id: "G-9NJRTDQEJ6" }
+}, router)
 
 new Vue({
   router,
